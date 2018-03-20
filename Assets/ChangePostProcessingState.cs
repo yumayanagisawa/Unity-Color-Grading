e428b694733dc7f3dca6f7ead8d96362214a71e4
Yuma Yanagisawa <ya7gisa0@gmail.com>
2018-03-20 14:41:12 +0900

    Initial commit

---
+++ Assets/ChangePostProcessingState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class ChangePostProcessingState : MonoBehaviour {
+    public PostProcessingProfile Profile;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            // check script is attached or not
+            PostProcessingBehaviour temp = gameObject.GetComponent<PostProcessingBehaviour>();
+            if (temp != null)
+            {
+                Debug.Log("post processing stack is attached.");
+                Destroy(temp);
+            }
+            else
+            {
+                gameObject.AddComponent<PostProcessingBehaviour>();
+                PostProcessingBehaviour behaviour = gameObject.GetComponent<PostProcessingBehaviour>();
+                behaviour.profile = Profile;
+            }
+        }
+	}
+}
